317aa536b8c9b45f2b177180524d9436d6c18752
Burak Demir <buraknasifdemir123@gmail.com>
2023-05-23 17:37:18 +0300

    make slider system editable and linked to pricing page

--- pages/_app.js
+++ pages/_app.js
@@ -35,8 +35,292 @@ function MyApp({ Component, pageProps }) {
   const [totalPrice, setTotalPrice] = useState(0);
 
   const [isOrder, setIsOrder] = useState(false);
-
-  console.log("isOrder", isOrder);
+  const [prices, setPrices] = useState([
+    {
+      planId: 1,
+      value: "1",
+      unit: "MB",
+      price: "1.70",
+      perPrice: [
+        {
+          value: "4 cents",
+          unit: "MB",
+        },
+        {
+          value: "2 cent",
+          unit: "SMS",
+        },
+      ],
+    },
+    {
+      planId: 2,
+      value: "10",
+      unit: "MB",
+      price: "1.95",
+      perPrice: [
+        {
+          value: "4 cents",
+          unit: "MB",
+        },
+        {
+          value: "2 cent",
+          unit: "SMS",
+        },
+      ],
+    },
+    {
+      planId: 3,
+      value: "25",
+      unit: "MB",
+      price: "2.45",
+      perPrice: [
+        {
+          value: "4 cents",
+          unit: "MB",
+        },
+        {
+          value: "2 cent",
+          unit: "SMS",
+        },
+      ],
+    },
+    {
+      planId: 4,
+      value: "50",
+      unit: "MB",
+      price: "2.95",
+      perPrice: [
+        {
+          value: "4 cents",
+          unit: "MB",
+        },
+        {
+          value: "2 cent",
+          unit: "SMS",
+        },
+      ],
+    },
+    {
+      planId: 5,
+      value: "100",
+      unit: "MB",
+      price: "3.45",
+      perPrice: [
+        {
+          value: "4 cents",
+          unit: "MB",
+        },
+        {
+          value: "2 cent",
+          unit: "SMS",
+        },
+      ],
+    },
+    {
+      planId: 6,
+      value: "150",
+      unit: "MB",
+      price: "4.45",
+      perPrice: [
+        {
+          value: "4 cents",
+          unit: "MB",
+        },
+        {
+          value: "2 cent",
+          unit: "SMS",
+        },
+      ],
+    },
+    {
+      planId: 7,
+      value: "250",
+      unit: "MB",
+      price: "5.45",
+      perPrice: [
+        {
+          value: "4 cents",
+          unit: "MB",
+        },
+        {
+          value: "2 cent",
+          unit: "SMS",
+        },
+      ],
+    },
+    {
+      planId: 8,
+      value: "500",
+      unit: "MB",
+      price: "7.45",
+      perPrice: [
+        {
+          value: "4 cents",
+          unit: "MB",
+        },
+        {
+          value: "2 cent",
+          unit: "SMS",
+        },
+      ],
+    },
+    {
+      planId: 9,
+      value: "750",
+      unit: "MB",
+      price: "9.95",
+      perPrice: [
+        {
+          value: "4 cents",
+          unit: "MB",
+        },
+        {
+          value: "2 cent",
+          unit: "SMS",
+        },
+      ],
+    },
+    {
+      planId: 10,
+      value: "1",
+      unit: "GB",
+      price: "11.95",
+      perPrice: [
+        {
+          value: "4 cents",
+          unit: "MB",
+        },
+        {
+          value: "2 cent",
+          unit: "SMS",
+        },
+      ],
+    },
+    {
+      planId: 11,
+      value: "2",
+      unit: "GB",
+      price: "23.95",
+      perPrice: [
+        {
+          value: "4 cents",
+          unit: "MB",
+        },
+        {
+          value: "2 cent",
+          unit: "SMS",
+        },
+      ],
+    },
+    {
+      planId: 12,
+      value: "3",
+      unit: "GB",
+      price: "34.95",
+      perPrice: [
+        {
+          value: "4 cents",
+          unit: "MB",
+        },
+        {
+          value: "2 cent",
+          unit: "SMS",
+        },
+      ],
+    },
+    {
+      planId: 13,
+      value: "4",
+      unit: "GB",
+      price: "45.95",
+      perPrice: [
+        {
+          value: "4 cents",
+          unit: "MB",
+        },
+        {
+          value: "2 cent",
+          unit: "SMS",
+        },
+      ],
+    },
+    {
+      planId: 14,
+      value: "5",
+      unit: "GB",
+      price: "56.45",
+      perPrice: [
+        {
+          value: "4 cents",
+          unit: "MB",
+        },
+        {
+          value: "2 cent",
+          unit: "SMS",
+        },
+      ],
+    },
+    {
+      planId: 15,
+      value: "10",
+      unit: "GB",
+      price: "109.95",
+      perPrice: [
+        {
+          value: "4 cents",
+          unit: "MB",
+        },
+        {
+          value: "2 cent",
+          unit: "SMS",
+        },
+      ],
+    },
+    {
+      planId: 16,
+      value: "25",
+      unit: "GB",
+      price: "159.95",
+      perPrice: [
+        {
+          value: "8 dollars",
+          unit: "GB",
+        },
+        {
+          value: "2 cent",
+          unit: "SMS",
+        },
+      ],
+    },
+    {
+      planId: 17,
+      value: "50",
+      unit: "GB",
+      price: "299.95",
+      perPrice: [
+        {
+          value: "8 dollars",
+          unit: "GB",
+        },
+        {
+          value: "2 cent",
+          unit: "SMS",
+        },
+      ],
+    },
+    {
+      planId: 18,
+      value: "NB",
+      unit: "-IOT",
+      price: "2.95",
+      perPrice: [
+        {
+          value: "1 cents",
+          unit: "MB",
+        },
+      ],
+    },
+  ]);
 
   return (
     <Layout>
@@ -48,6 +332,7 @@ function MyApp({ Component, pageProps }) {
         setTotalPrice={setTotalPrice}
         isOrder={isOrder}
         setIsOrder={setIsOrder}
+        prices={prices}
         {...pageProps}
       />
     </Layout>
